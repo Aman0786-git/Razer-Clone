395eb20d967aa82176b0b8f1fe99f421629d9555
Aman <aman999.au@gmail.com>
2022-10-16 12:46:08 +0530

    update

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 const searchBtn = document.querySelector("#search--Btn");
 const searchBox = document.querySelector("#search--box");
 const closeBtn = document.querySelector("#close--Btn");
-
 const cartBtn = document.querySelector("#cart--Btn");
+const cartContainer = document.querySelector(".cartContainer");
+const bodyArea = document.querySelector("#body");
 
-searchBtn.addEventListener("click", () => {
+const addActiveSearch = () => {
   searchBtn.classList.add("active");
   searchBox.classList.add("active");
   closeBtn.classList.add("active");
-});
+  cartContainer.classList.remove("active");
+};
 
-closeBtn.addEventListener("click", () => {
+const removeActiveSearch = () => {
   searchBtn.classList.remove("active");
   closeBtn.classList.remove("active");
   searchBox.classList.remove("active");
+};
+
+const removeCart = () => {
+  cartContainer.classList.remove("active");
+};
+
+const removeActiveAll = () => {
+  removeActiveSearch();
+};
+
+searchBtn.addEventListener("click", () => {
+  addActiveSearch();
+});
+
+closeBtn.addEventListener("click", () => {
+  removeActiveAll();
+});
+
+cartBtn.addEventListener("click", () => {
+  if (searchBox.classList.contains("active")) removeActiveSearch();
+  if (cartContainer.classList.contains("active")) {
+    removeCart();
+  } else cartContainer.classList.add("active");
 });
